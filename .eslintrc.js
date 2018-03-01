bf201f6318d814a222c3a42faa1300b8e1846b95
Sylvan Mably <smably@ebay.com>
2018-02-28 20:14:12 -0500

    Move prettier config to .prettierrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,8 @@
 module.exports = {
-  "parser": "babel-eslint",
-  "extends": [
-    "airbnb-base",
-    "plugin:flowtype/recommended",
-    "prettier"
-  ],
-  "plugins": [
-    "flowtype",
-    "import",
-    "prettier"
-  ],
-  "rules": {
-    "no-plusplus": "off",
-    "prettier/prettier": ["error", { "printWidth": 120, "singleQuote": true, "trailingComma": "all" }]
+  parser: "babel-eslint",
+  extends: ["airbnb-base", "plugin:flowtype/recommended", "prettier"],
+  plugins: ["flowtype", "import", "prettier"],
+  rules: {
+    "no-plusplus": "off"
   }
 };
